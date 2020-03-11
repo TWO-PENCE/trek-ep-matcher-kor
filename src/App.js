d784ee0695ea2ea39e976724075abe7aa71ab7ff
TWO-PENCE <toromstar@gmail.com>
2020-03-11 18:01:53 +0900

    git 유저 정보 확인용 커밋

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Main from "./components/Main";
-import STapi from "./components/STapi";
+import Main from "./pages/Main";
+import STapi from "./pages/STapi";
 import "./css/scss/App.scss";
 
 class App extends Component {
